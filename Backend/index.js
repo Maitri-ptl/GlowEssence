3a426691f2fae17c2f1f105c2683bc3cd7014f03
kashishsevak <kashishsevak26@gmail.com>
2026-07-29 00:47:39 +0530

    CORS added

--- Backend/index.js
+++ Backend/index.js
@@ -4,10 +4,17 @@ import db from "./configs/db.js";
 import bodyParser from "body-parser";
 import router from "./routes/index.js";
 import razorpay from "./configs/razorpay.js";
+import cors from "cors";
 
 const port = process.env.PORT || 3000;
 
 const app = express();
+
+app.use(cors({
+    origin: "https://glow-essence-tau.vercel.app",
+    credentials: true,
+}));
+
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
